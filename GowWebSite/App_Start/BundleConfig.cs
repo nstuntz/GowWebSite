190cfc9b6c964acb12361c4af6958344e1375dea
nstuntz <nathaniel@stuntz.org>
2015-06-25 00:19:17 -0400

    Pause/Update

--- GowWebSite/App_Start/BundleConfig.cs
+++ GowWebSite/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace GowWebSite
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                    "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
