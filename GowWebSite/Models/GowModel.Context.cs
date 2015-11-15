2de8dec41a24d2c3ff1fb77c55466fc59b97d0a6
GraysonStuntz <GStuntz@MCHOLT-50.mcho.local>
2015-11-15 11:17:01 -0500

    Updated to Show MachineTracker on Log Page

--- GowWebSite/Models/GowModel.Context.cs
+++ GowWebSite/Models/GowModel.Context.cs
@@ -39,6 +39,7 @@ namespace GowWebSite.Models
         public virtual DbSet<Subscription> Subscriptions { get; set; }
         public virtual DbSet<UserDemographic> UserDemographics { get; set; }
         public virtual DbSet<UserPayItem> UserPayItems { get; set; }
+        public virtual DbSet<MachineLoginTracker> MachineLoginTrackers { get; set; }
     
         public virtual int CreateExitingCitySetup(string userName, string password, string cityName, Nullable<int> cityX, Nullable<int> cityY, Nullable<int> allianceID, Nullable<int> rSSType, Nullable<int> sHLevel)
         {
